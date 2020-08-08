8f51a09ea5a94b2bece9f727a3c7ddf0ab56431d
miftah135791 <68943355+miftah135791@users.noreply.github.com>
2020-08-08 10:40:03 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+ import React, {Component} from 'react';
+import {AppRegistry} from 'react-native';
+// import App from './App';
+import {name as appName} from './app.json';
+import BasicFlatListData from './components/BasicFlatListData';
+import Home from './components/Home';
+
+AppRegistry.registerComponent(appName, () => BasicFlatListData);
